4c536ad33944279a44ca0287757ab777fffaf0be
yuriquezada <quezadat.yuri@gmail.com>
2022-11-16 08:24:44 -0500

    Correct Vertical stepper

--- src/components/VerticalStepper.jsx
+++ src/components/VerticalStepper.jsx
@@ -4,48 +4,58 @@ import '../style/VerticalStepper.css';
 export default function VerticalLinearStepper() {
 
   return (
-        <div class="vertical-stepper">
-            <div class="step completed">
-              <div class="v-stepper">
-                <div class="circle"></div>
-                <div class="line"></div>
+        <div className="vertical-stepper">
+            <div className="step completed">
+              <div className="v-stepper">
+                <div className="circle"></div>
+                <div className="line"></div>
               </div>
 
-              <div class="content">
-                Paso 1
+              <div className="content">
+                Datos de la aplicación
               </div>
           </div>
           
-          <div class="step active">
-            <div class="v-stepper">
-              <div class="circle"></div>
-              <div class="line"></div>
+          <div className="step active">
+            <div className="v-stepper">
+              <div className="circle"></div>
+              <div className="line"></div>
             </div>
 
-            <div class="content">
-              Paso 2
+            <div className="content">
+              Deudas y servicios
             </div>
           </div>
           
-          <div class="step empty">
-              <div class="v-stepper">
-                <div class="circle"></div>
-                <div class="line"></div>
+          <div className="step empty">
+              <div className="v-stepper">
+                <div className="circle"></div>
+                <div className="line"></div>
               </div>
 
-              <div class="content">
-                Paso 3
+              <div className="content">
+                Integraciones y base de datos
               </div>
           </div>
           
-          <div class="step">
-              <div class="v-stepper">
-                <div class="circle"></div>
-                <div class="line"></div>
+          <div className="step">
+              <div className="v-stepper">
+                <div className="circle"></div>
+                <div className="line"></div>
               </div>
 
-              <div class="content">
-                Paso 4
+              <div className="content">
+                Matriz de madurez
+              </div>
+          </div>
+          <div className="step">
+              <div className="v-stepper">
+                <div className="circle"></div>
+                <div className="line"></div>
+              </div>
+
+              <div className="content">
+                Cloudificación
               </div>
           </div>
           
