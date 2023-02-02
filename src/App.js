857c692cda9e15e3750d64157297305919b511d5
yuriquezada <quezadat.yuri@gmail.com>
2023-02-02 17:16:44 -0500

    Add 3 addicional pages

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import { FirstStep } from './Pages/FirstStep';
 import { SecondStep } from './Pages/SecondStep';
-import Header from './components/Header';
+import { ThirdStep } from './Pages/ThirdStep';
+import { FourthStep } from './Pages/FourthStep';
+import { FiveStep } from './Pages/FiveStep';
+import { Confirmation } from './Pages/Confirmation';
 
+import Header from './components/Header';
 import { Authenticator } from '@aws-amplify/ui-react';
+
+
 
 
 const App = () => {
@@ -21,6 +27,10 @@ const App = () => {
               <Route path="/" element={<Home/>} />
               <Route path="/step1" element={<FirstStep/>} />
               <Route path="/step2" element={<SecondStep/>} />
+              <Route path="/step3" element={<ThirdStep/>} />
+              <Route path="/step4" element={<FourthStep/>} />
+              <Route path="/step5" element={<FiveStep/>} />
+              <Route path="/confirmation" element={<Confirmation/>} />
             </Routes>
           </div>
         )}
